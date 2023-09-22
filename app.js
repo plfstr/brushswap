33b7294c0731694975c3d4eeddc954a4e56ef249
Paul <plfstr@users.noreply.github.com>
2023-09-22 18:55:31 +0100

    PWA install prompt delay

--- app.js
+++ app.js
@@ -237,6 +237,7 @@ function brushSwapped() {
 				console.error(error);
 			});
 		}
+		deferredPrompt.prompt();
 	} catch(err) {
 		if (err instanceof ReferenceError) { 
 			userMsg('Data Save Failed! Check browser cookie and storage settings and retry');
@@ -308,3 +309,12 @@ if (navigator.setAppBadge && storedDate && dayjs(storedDate).add(90, 'day') < da
       console.error(error);
     });
 }
+
+/**
+* Delay PWA install prompt. Call after brush swap...
+*/
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+});
